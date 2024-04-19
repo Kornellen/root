ac14ab9b65167853f73786948be0854c39775c1f
kornellen <kornel.konopka@onet.pl>
2024-04-19 22:45:18 +0200

    changed profiles.sql

--- backend/database/profiles.sql
+++ backend/database/profiles.sql
@@ -1,19 +1,93 @@
-create database if not exists Profiles;
-use Profiles;
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Apr 19, 2024 at 10:44 PM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
 
 
-create table logins (
-    userID int primary key,
-    username varchar(55) collate utf8mb4_bin,
-    email varchar(55) collate utf8mb4_bin,
-    password varchar(64) collate utf8mb4_bin);
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
 
+--
+-- Database: `profiles`
+--
 
+-- --------------------------------------------------------
 
+--
+-- Table structure for table `logins`
+--
 
-create table userdata (
-    dataID int primary key auto_increment,
-    userID int,
-    dataType enum('config', 'other', 'userData'),
-    dataData varchar(150));
+CREATE TABLE `logins` (
+  `userID` int(11) NOT NULL,
+  `username` varchar(55) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin DEFAULT NULL,
+  `email` varchar(55) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin DEFAULT NULL,
+  `password` varchar(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+--
+-- Dumping data for table `logins`
+--
+
+INSERT INTO `logins` (`userID`, `username`, `email`, `password`) VALUES
+(1, 'admin', 'admin@admin.com', '8c6976e5b5410415bde908bd4dee15dfb167a9c873fc4bb8a81f6f2ab448a918');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `userdata`
+--
+
+CREATE TABLE `userdata` (
+  `dataID` int(11) NOT NULL,
+  `userID` int(11) DEFAULT NULL,
+  `dataType` enum('config','other','userData') DEFAULT NULL,
+  `dataData` varchar(150) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `userdata`
+--
+
+INSERT INTO `userdata` (`dataID`, `userID`, `dataType`, `dataData`) VALUES
+(1, 24997182, 'other', 'admin');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `logins`
+--
+ALTER TABLE `logins`
+  ADD PRIMARY KEY (`userID`);
+
+--
+-- Indexes for table `userdata`
+--
+ALTER TABLE `userdata`
+  ADD PRIMARY KEY (`dataID`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `userdata`
+--
+ALTER TABLE `userdata`
+  MODIFY `dataID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
