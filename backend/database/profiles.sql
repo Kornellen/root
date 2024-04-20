68a2bb9743d1566c5ea2230e9989d60e79f32215
kornellen <kornel.konopka@onet.pl>
2024-04-20 19:36:55 +0200

    Fix

--- backend/database/profiles.sql
+++ backend/database/profiles.sql
@@ -41,7 +41,7 @@ CREATE TABLE `logins` (
 
 INSERT INTO `logins` (`userID`, `username`, `email`, `password`, `verified`) VALUES
 (24997182, 'admin', 'admin@admin.com', '8c6976e5b5410415bde908bd4dee15dfb167a9c873fc4bb8a81f6f2ab448a918', 0),
-(90670236, 'okok', 'kornel.konopka@onet.pl', '5994471abb01112afcc18159f6cc74b4f511b99806da59b3caf5a9c173cacfc5', 0);
+
 
 -- --------------------------------------------------------
 
